ac3af409129fa7d577a8c8a06c6e81b2f74ef807
DarkPro2102 <diego_alejandro2000@hotmail.com>
2021-05-20 23:20:17 -0500

    Coefficients function done and graph function complete

--- Linear_Regression_Algorithm.py
+++ Linear_Regression_Algorithm.py
@@ -1,11 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def estimate_coef():
+def estimate_coef(x,y):
     n = np.size(x)
 
     mean_x, mean_y = np.mean(x), np.mean(y)
     
     #Implementation of minimun square method
-    
+    summatory_xy = np.sum((x-mean_x)*(y-mean_y))
+    summatory_xx = np.sum(x*(x-mean_x))
 
+    #Coefficients
+    b1 = summatory_xy/summatory_xx
+    b0 = mean_y - b1*mean_x
+
+    return(b0,b1)
+    
+#Graph function
+def plot_regression(x,y,b):
+    plt.scatter(x, y, color = 'g', marker= 'o', s = 30)
+    
+    y_pred = b[0] + b[1]*x
+    plt.plot(x, y_pred, color = 'b')
+
+    #Labels
+    plt.xlabel('x - Independent')
+    plt.ylabel('y - Dependent')
+
+    plt.show()
