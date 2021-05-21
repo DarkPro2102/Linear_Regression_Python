c090ebf795e9ee3d2938bbab85190409dd0b8150
DarkPro2102 <diego_alejandro2000@hotmail.com>
2021-05-20 22:59:37 -0500

    First commit.

---
+++ Linear_Regression_Algorithm.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def estimate_coef():
+    n = np.size(x)
+
+    mean_x, mean_y = np.mean(x), np.mean(y)
+    
+    #Implementation of minimun square method
+    
+
